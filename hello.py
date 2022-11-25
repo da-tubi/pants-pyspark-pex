d0f7488d12e63bbc7e0c539831fa234d46e894a2
Da Shen <da@tubi.tv>
2022-11-25 14:06:31 +0800

    Use functions

--- hello.py
+++ hello.py
@@ -7,29 +7,26 @@ from pyspark.sql.functions import col, pandas_udf
 from pyspark.sql.types import LongType
 from pyspark.sql import SparkSession
 
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-
-spark = SparkSession.builder.getOrCreate()
 
 # Declare the function and create the UDF
 def multiply_func(a: pd.Series, b: pd.Series) -> pd.Series:
     return a * b
 
-multiply = pandas_udf(multiply_func, returnType=LongType())
+def run_pandas_udf(spark):
+    print(f"We are using Pandas {pd.__version__}")
+    multiply = pandas_udf(multiply_func, returnType=LongType())
+    x = pd.Series([1, 2, 3])
+    print(multiply_func(x, x))
+    
+    df = spark.createDataFrame(pd.DataFrame(x, columns=["x"]))
+    df.select(multiply(col("x"), col("x"))).show()
 
-print(f"We are using Pandas {pd.__version__}")
+def main():
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    
+    spark = SparkSession.builder.getOrCreate()
+    run_pandas_udf(spark)
 
-# The function for a pandas_udf should be able to execute with local Pandas data
-x = pd.Series([1, 2, 3])
-print(multiply_func(x, x))
-# 0    1
-# 1    4
-# 2    9
-# dtype: int64
-
-# Create a Spark DataFrame, 'spark' is an existing SparkSession
-df = spark.createDataFrame(pd.DataFrame(x, columns=["x"]))
-
-# Execute function as a Spark vectorized UDF
-df.select(multiply(col("x"), col("x"))).show()
+if __name__ == "__main__":
+    main()
