4b46e7e8e0b5edd173b74baeb1334d41dac51e50
Da Shen <da@tubi.tv>
2022-11-25 16:50:40 +0800

    Final

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-import os
-import sys
-
 import pandas as pd
-
-from pyspark.sql.functions import col, pandas_udf
-from pyspark.sql.types import LongType
-from pyspark.sql import SparkSession
-
 
 # Declare the function and create the UDF
 def multiply_func(a: pd.Series, b: pd.Series) -> pd.Series:
     return a * b
 
 def run_pandas_udf(spark):
+    from pyspark.sql.functions import col, pandas_udf
+    from pyspark.sql.types import LongType
     print(f"We are using Pandas {pd.__version__}")
     multiply = pandas_udf(multiply_func, returnType=LongType())
     x = pd.Series([1, 2, 3])
@@ -21,12 +15,13 @@ def run_pandas_udf(spark):
     df = spark.createDataFrame(pd.DataFrame(x, columns=["x"]))
     df.select(multiply(col("x"), col("x"))).show()
 
-def main():
-    os.environ['PYSPARK_PYTHON'] = sys.executable
-    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-    
-    spark = SparkSession.builder.getOrCreate()
-    run_pandas_udf(spark)
-
 if __name__ == "__main__":
-    main()
+    import os
+    from pyspark.sql import SparkSession
+    
+    os.environ['PYSPARK_PYTHON'] = "./dist/app.pex"
+    spark = SparkSession.builder.config(
+        "spark.files",  # 'spark.yarn.dist.files' in YARN.
+        "dist/app.pex"
+    ).getOrCreate()
+    run_pandas_udf(spark)
