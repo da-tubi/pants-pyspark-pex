72f16f4851d2bf68afcc45440219dc90db38ca08
Da Shen <da@tubi.tv>
2022-11-25 11:44:24 +0800

    pandas udf

--- hello.py
+++ hello.py
@@ -1,11 +1,35 @@
-import pandas
+import os
+import sys
 
-def main():
-    try:
-        import pyspark
-        print(f"PySpark version {pyspark.__version__}")
-    except:
-        print("PySpark not available")
+import pandas as pd
 
-    print(f"Hello with Pandas {pandas.__version__}")
+from pyspark.sql.functions import col, pandas_udf
+from pyspark.sql.types import LongType
+from pyspark.sql import SparkSession
 
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+spark = SparkSession.builder.getOrCreate()
+
+# Declare the function and create the UDF
+def multiply_func(a: pd.Series, b: pd.Series) -> pd.Series:
+    return a * b
+
+multiply = pandas_udf(multiply_func, returnType=LongType())
+
+print(f"We are using Pandas {pd.__version__}")
+
+# The function for a pandas_udf should be able to execute with local Pandas data
+x = pd.Series([1, 2, 3])
+print(multiply_func(x, x))
+# 0    1
+# 1    4
+# 2    9
+# dtype: int64
+
+# Create a Spark DataFrame, 'spark' is an existing SparkSession
+df = spark.createDataFrame(pd.DataFrame(x, columns=["x"]))
+
+# Execute function as a Spark vectorized UDF
+df.select(multiply(col("x"), col("x"))).show()
